d9bda48ebfb498250697c33926cf54a94a2ff39c
fniemeyer <fniemeyer@techfak.uni-bielefeld.de>
2022-01-29 15:31:28 +0100

    Finalize atoi method

--- arabic2roman.asm
+++ arabic2roman.asm
@@ -235,7 +235,7 @@ _start:
 read_args:
 	;; print command line arguments
 	pop	rsi					; argv[j]
-	mov r10, byte [rsi]
+	mov r10, dword [rsi]
 	call	write_char
 	call	string_len		; get string length
 	call 	atoi			; convert given string to int and store it in r15
