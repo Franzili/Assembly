a80aa0728610c09f07c47abc0979ff4f137a73e2
fniemeyer <fniemeyer@techfak.uni-bielefeld.de>
2022-01-29 16:47:09 +0100

    Finalize atoi method

--- arabic2roman.asm
+++ arabic2roman.asm
@@ -23,10 +23,10 @@ blank:
     db 0x20
 ;;; begin decimals in ASCII
 ascii0:
-	dd 48
+	db 48
 ;;; end decimals in ASCII
 ascii9:
-	dd 57
+	db 57
 ;;; roman numerals
 numeral1: 		db 'I'
 numeral5: 		db 'V'
@@ -191,17 +191,17 @@ atoi:
 convert_atoi:
 	cmp [rsi], byte 0
 	je exit_atoi				; end of string found
-	cmp dword [rsi], ascii0		; decimal < 0 ? -> not a number
+	cmp byte [rsi], ascii0		; decimal < 0 ? -> not a number
 	jl not_a_number
-	cmp dword [rsi], ascii9		; decimal > 9 ? -> not a number
+	cmp byte [rsi], ascii9		; decimal > 9 ? -> not a number
 	jg not_a_number
 
 	; digit between 0 and 9
-	sub dword [rsi], ascii0		; convert char to decimal
-	mov rax, [rsi]
+	sub byte [rsi], ascii0		; convert char to decimal
+	mov rax, byte [rsi]
 	mov r14, 10
 	mul r14						; next digit -> mul 10
-	add r15, [rsi]				; store next digit in r15
+	add r15, byte [rsi]				; store next digit in r15
 	mov r10, r15
 	call write_char
 	inc rsi						; next position in string
