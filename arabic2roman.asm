b8194302dadabece18bb8788bf33796697b01651
fniemeyer <fniemeyer@techfak.uni-bielefeld.de>
2022-01-29 15:37:31 +0100

    Finalize atoi method

--- arabic2roman.asm
+++ arabic2roman.asm
@@ -211,7 +211,7 @@ not_a_number:
 	pop r14
 	pop rax
 	pop rsi
-	mov r15, 'c'					; result -1 if input not a number
+	mov r15, -1					; result -1 if input not a number
 	mov r10, debug
 	call write_char
 	mov r10, r15
