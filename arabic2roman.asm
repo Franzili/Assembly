a876a9c3ed88755c7b8602437a53823594326621
fniemeyer <fniemeyer@techfak.uni-bielefeld.de>
2022-01-29 16:01:34 +0100

    Finalize atoi method

--- arabic2roman.asm
+++ arabic2roman.asm
@@ -201,7 +201,7 @@ convert_atoi:
 	mov rax, [rsi]
 	mov r14, 10
 	mul r14						; next digit -> mul 10
-	add r15, [rax]				; store next digit in r15
+	add r15, [rsi]				; store next digit in r15
 	mov r10, r15
 	call write_char
 	inc rsi						; next position in string
