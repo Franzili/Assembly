81794394043295cb8428b91c0eb476bc8dad73f7
fniemeyer <fniemeyer@techfak.uni-bielefeld.de>
2022-01-30 15:46:22 +0100

    Beautify schraeg

--- schraeg.asm
+++ schraeg.asm
@@ -41,19 +41,19 @@ write_char:
     push    r8
 	push    rcx
 	;; prepare arguments for write syscall
-	mov	rax, SYS_WRITE	; write syscall
-	mov	rdi, STDOUT		; file descriptor = 1 (stdout)
-	mov	rsi, r8			; character to write
-	mov	rdx, 1			; length
+	mov		rax, SYS_WRITE	; write syscall
+	mov		rdi, STDOUT		; file descriptor = 1 (stdout)
+	mov		rsi, r8			; character to write
+	mov		rdx, 1			; length
 	syscall				; system call
 	;; restore registers (in opposite order)
-	pop rcx
-    pop r8
-    pop r9
-	pop	rdx
-	pop	rsi
-	pop	rdi
-	pop	rax
+	pop 	rcx
+    pop 	r8
+    pop 	r9
+	pop		rdx
+	pop		rsi
+	pop		rdi
+	pop		rax
 	ret
 
 
@@ -65,36 +65,36 @@ write_string:
 	push	rax
 	push	rdi
 	push	rdx
-    mov rcx, 0  		; position in string
+    mov 	rcx, 0  		; position in string
     push    rcx
-    push    r9  		; inner loop variable
+    push    r9  			; inner loop variable
 writing_loop:
-    cmp [rsi], byte 0
-    je eos_found
-    mov r8, blank
-    mov r9, 0   		; inner loop variable
+    cmp 	[rsi], byte 0
+    je 		eos_found
+    mov 	r8, blank
+    mov 	r9, 0   			; inner loop variable
 blank_loop:
-    cmp r9, rcx
-    jge write_one_char
-    call write_char
-    inc r9
-    jmp blank_loop
+    cmp 	r9, rcx
+    jge 	write_one_char
+    call 	write_char
+    inc 	r9
+    jmp 	blank_loop
 write_one_char:
-    mov r8, rsi
-    call write_char
-    mov r8, newline    	; newline
-    call write_char
-    inc rcx     		; current position in string
-	inc	rdx				; count
-	inc	rsi				; next position in string
-    jmp writing_loop
+    mov 	r8, rsi
+    call 	write_char
+    mov 	r8, newline    	; newline
+    call 	write_char
+    inc 	rcx     		; current position in string
+	inc		rdx				; count
+	inc		rsi				; next position in string
+    jmp 	writing_loop
 eos_found:
 	;; here rdx contains the string length
-    pop r9
-    pop rcx
-    pop	rdx
-	pop	rdi
-	pop	rax
+    pop 	r9
+    pop 	rcx
+    pop		rdx
+	pop		rdi
+	pop		rax
 	ret
 
 
@@ -103,18 +103,18 @@ eos_found:
 ;;;--------------------------------------------------------------------------
 
 _start:
-	pop	rbx					; argc (>= 1 guaranteed)
-	pop	rsi					; argv[j]
-	dec rbx
-	jz exit
+	pop		rbx					; argc (>= 1 guaranteed)
+	pop		rsi					; argv[j]
+	dec 	rbx
+	jz 		exit
 read_args:
 	;; print command line arguments
-	pop	rsi					; argv[j]
-	call	write_string	; string in rsi is written to stdout
-	dec	rbx					; dec arg-index
-	jnz	read_args			; continue until last argument was printed
+	pop		rsi					; argv[j]
+	call	write_string		; string in rsi is written to stdout
+	dec		rbx					; dec arg-index
+	jnz		read_args			; continue until last argument was printed
 exit:
 	;; exit program via syscall exit (necessary!)
-	mov	rax, SYS_EXIT		; exit syscall
-	mov	rdi, 0				; exit code 0 (= "ok")
-	syscall 				; kernel interrupt: system call
+	mov		rax, SYS_EXIT		; exit syscall
+	mov		rdi, 0				; exit code 0 (= "ok")
+	syscall 					; kernel interrupt: system call
