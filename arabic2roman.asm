4215c3d369af231590918a08a1ba1b9547360579
fniemeyer <fniemeyer@techfak.uni-bielefeld.de>
2022-01-30 12:39:29 +0100

    Finalize atoi method

--- arabic2roman.asm
+++ arabic2roman.asm
@@ -115,12 +115,13 @@ equal2_4:				; digit equal to 4
 	call write_char
 
 smaller_4:
+	mov r10, r11
+smaller_4_loop:
 	cmp r9, 0
 	jle exit_block		; exit when 0 reached
-	mov r10, r11
 	call write_char
 	dec r9
-	jmp smaller_4
+	jmp smaller_4_loop
 
 exit_block:
 	pop r9
